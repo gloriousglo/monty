6cc35ff7d0b1ad80ab3268996c9fbb1cf70ae7cd
gloriousglo <gloriousg89@gmail.com>
2023-12-20 00:09:38 -0800

    monty

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+/**
+ * function_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void function_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
+	if (bus.arg)
+{
+	if (bus.arg[0] == '-')
+		j++;
+	for (; bus.arg[j] != '\0'; j++)
+	{
+		if (bus.arg[j] > 57 || bus.arg[j] < 48)
+			is_valid = 1; 
+	}
+	if (is_valid == 1)
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+       	}
+}
+ 	else
+	{ 
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); 
+	}
+	number = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, number);
+	else
+		addqueue(head, number);
+}
